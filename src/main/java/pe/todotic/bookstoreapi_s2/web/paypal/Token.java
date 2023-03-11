d1942205f64aa866d0058506bc346c0747e735ee
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-03-10 23:29:18 -0500

    ...

--- src/main/java/pe/todotic/bookstoreapi_s2/web/paypal/Token.java
+++ src/main/java/pe/todotic/bookstoreapi_s2/web/paypal/Token.java
@@ -1,4 +1,8 @@
 package pe.todotic.bookstoreapi_s2.web.paypal;
 
 public class Token {
+    private String acces_token;
+    private String token_type;
+    private String app_id;
+    private Long expires_in;
 }
